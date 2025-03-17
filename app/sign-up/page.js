dfe9bee2ca73aa6b265b2834db8ede7a8aae3ef4
tenshicakes <jmontalan8@gmail.com>
2025-03-17 14:19:46 +0800

    modified sign up page

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className="min-h-screen bg-[#E8F5E9] flex justify-between items-center">
+
+      
+    <div className="flex flex-col items-start ml-[15rem] mr-[15rem]">
+      <h1 className="text-[1.5rem] text-black ml-[2.8rem] font-bold" style={{ fontFamily: '"Segoe UI", sans-serif' }} >VeriFace Track</h1>
+      <p className="text-[1rem] text-[#ACACAC]" style={{ fontFamily: '"Segoe UI", sans-serif' }}>With this App Your Safety is Secured</p>
+    </div>
+
+<div className=" items-end bg-white w-[30rem] h-[33rem] m-[1rem] rounded-[15px] "> </div>
+
+
+</div>  
   )
 }
 
